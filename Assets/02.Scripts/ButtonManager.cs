f96cc49c557077c4d51d5f9d3050645c712a3261
Yongjung Shin <syj4209@naver.com>
2021-05-11 17:08:25 +0900

    ARScene Update

--- Assets/02.Scripts/ButtonManager.cs
+++ Assets/02.Scripts/ButtonManager.cs
@@ -6,6 +6,18 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour
 {
     public GameObject ShipGroup;
+    public GameObject flowerCharPref;
+
+    private List<GameObject> targetObjList = new List<GameObject>();
+
+    private GameObject selectedTarget;
+
+    private Vector2 camCenterPos;
+
+    public void Start()
+    {
+        camCenterPos = new Vector2(Screen.width/2, Screen.height);
+    }
 
     public void OnBuyButtonClick()
     {
@@ -17,5 +29,58 @@ public class ButtonManager : MonoBehaviour
     {
         SceneManager.LoadScene("ARScene");
     }
+
+
+    public void OnInfoClick()
+    {
+        
+    }
+
+    public void OnCaptureClick()
+    {
+
+    }
+
+    public void OnGrowthClick()
+    {
+
+    }
+
+    public void OnAnimClick()
+    {
+    //     if(flowerCharPref.activeSelf)
+    //     {
+    //         flowerCharPref.SetActive(false);
+    //     }
+    //     else
+    //     {
+    //         flowerCharPref.SetActive(true);
+    //     }
+    }
+
+    public void OnVuTargetFound(GameObject newTarget)
+    {
+        if(!targetObjList.Contains(newTarget))
+        {
+            targetObjList.Add(newTarget);
+        }
+    }
     
+    public void OnVuTargetLost(GameObject oldTarget)
+    {
+        if(targetObjList.Contains(oldTarget))
+        {
+            targetObjList.Remove(oldTarget);
+        }
+    }
+
+    public void SearchTargetInArea()
+    {
+        GameObject nearestObject;
+
+        foreach(var obj in targetObjList)
+        {
+            Debug.Log(obj.name);
+        }
+    }
 }
